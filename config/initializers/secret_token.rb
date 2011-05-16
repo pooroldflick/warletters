bf2ee84f9a519e2388e98ebf1a7bf6b96ce0fb4c
Kevin Flick <pooroldflick@gmail.com>
2011-05-16 12:28:15 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Warletters::Application.config.secret_token = '760311ed7cd56401bee8b3af3d913faf8f3dd49be0bb90df7893f575844c5070ae624268d3f0ab91be2adbfe33b678b2db7de3d7f6a4dfcb9c118a4eb9f1c2a3'
