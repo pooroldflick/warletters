6b8d7301e8ccf0db41f95be4d4a0f75b06aebf16
Kevin Flick <pooroldflick@gmail.com>
2011-05-21 11:09:13 -0400

    Added password and encrypted_password attributes to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,32 @@
 # == Schema Information
-# Schema version: 20110516183001
+# Schema version: 20110521150302
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
 class User < ActiveRecord::Base
-  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i		# regular expression of valid email addresses
+  attr_accessor   :password		# creates a password attribute
+  attr_accessible :name, :email, :password, :password_confirmation		# allows users to edit their name and email
 
-  has_many :letters						# Users have many letters
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i				# regular expression of valid email addresses
 
-  validates_presence_of :name,  :presence   => true, 		 	# requires users to enter a name
-				:length     => { :maximum => 50 } 	# limits the length of names to 50
-  validates_presence_of :email, :presence   => true,	  	  	# requires users to enter an email
+  has_many :letters								# Users have many letters
+
+  validates		        :name,  :presence      => true, 		# requires users to enter a name
+				:length     => { :maximum => 50 } 		# limits the length of names to 50
+
+  validates			 :email, :presence     => true,	  	  	# requires users to enter an email
 				:format     => { :with => email_regex },	# valid email
  				:uniqueness => { :case_sensitive => false }   	# :uniqueness => true is implicit
-  attr_accessible :name, :email					  	# allows users to edit their name and email
+
+  validates			:password, :presence     => true,		# validates the presence of a password
+					   :confirmation => true,		# confirms and creates a password confirmation attribute
+					   :length	 => { :within => 6..40 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110521002700) do
+ActiveRecord::Schema.define(:version => 20110521150302) do
 
   create_table "letters", :force => true do |t|
     t.date     "datewritten"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20110521002700) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email"
